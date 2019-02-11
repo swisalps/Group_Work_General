ea58d16a6716daabd48c93002551cd1eadf81c64
sjhalloran2 <sjhalloran@csbsju.edu>
2019-02-11 21:14:10 +0000

    comments

--- SolitaireSimulation/MVPSolitaire/Card.cpp
+++ SolitaireSimulation/MVPSolitaire/Card.cpp
@@ -6,9 +6,12 @@ class Card {
 int num;
 std::string suit;
 bool isVis;
-    Card(int number, std::string cardSuit){
+//constructs a card instance made up of the cards number,
+//suit and whether its value is visible to the user or not
+    Card(int number, std::string cardSuit, bool visible){
         num = number;
         suit = cardSuit;
+        isVis = visible;
     }
     int getNum(){
         return num;
