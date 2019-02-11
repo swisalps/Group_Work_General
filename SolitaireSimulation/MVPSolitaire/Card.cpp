ccb562be5b96480f2882298b1d87b79104b4df96
Will Haanen <dawiller34@hotmail.com>
2019-02-11 21:06:50 +0000

    Set up Card Class

--- SolitaireSimulation/MVPSolitaire/Card.cpp
+++ SolitaireSimulation/MVPSolitaire/Card.cpp
@@ -1,44 +1,49 @@
 #include <stdlib.h>     /* srand, rand */
-#include "card.h"
 #include <iostream>
 
 
-const std::string SUIT[SUIT_MAX]  = {"S", "H", "D", "C"};
-const std::string RANK[RANK_MAX]  = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+class Card {
+int num;
+std::string suit;
+bool isVis;
+    Card(int number, std::string cardSuit){
+        num = number;
+        suit = cardSuit;
+    }
+    int getNum(){
+        return num;
+    }
+     std::string getSuit(){
+         return suit;
+     }
+     bool isRed(){
+         if ((suit != "S") || (suit != "C")){
+             return true;
+         }
 
-
-Card::Card()
-{
-   m_suit = generate_suit();
-   m_rank = generate_rank();
+         else{
+             return false;
+         }
+     }
+     bool getVisible(){
+         return isVis;
+     }
+     void setVis(){
+         isVis = true;
+     }
+     void setNotVis(){
+         isVis = false;
+     }
+    //Compares lower value card to higher
+    //Must be called on lower value card
+     bool compareCardLow(Card otherCard){
+         if ((num = otherCard.getNum() + 1)&&(this->isRed() != otherCard.isRed())){
+             return true;
+         }
+         else{
+             return false;
+         }
+     }
 }
 
-Card::Card(const int &suit, const int &rank) : m_suit(suit), m_rank(rank)
-{
 
-}
-
-int Card::generate_suit()
-{
-    return rand() % (SUIT_MAX-1) + 0;
-}
-
-int Card::generate_rank()
-{
-    return rand() % (RANK_MAX-1) + 0;
-}
-
-std::string Card::Card2Str() const
-{
-    return SUIT[get_suit()] + RANK[get_rank()];
-}
-
-int Card::get_suit() const
-{
-    return m_suit;
-}
-
-int Card::get_rank() const
-{
-    return m_rank;
-}
