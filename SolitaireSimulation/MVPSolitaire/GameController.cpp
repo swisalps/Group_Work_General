b7c302e3b56d6466a0108a6b275e835593cfed3c
Ian Bush <IBUSH001@csbsju.edu>
2019-02-15 18:21:29 +0000

    expanding methods

--- SolitaireSimulation/MVPSolitaire/GameController.cpp
+++ SolitaireSimulation/MVPSolitaire/GameController.cpp
@@ -1,12 +1,14 @@
-#include "Card.cpp"
 #include "cardDeck.cpp"
 #include <vector>
 #include <stdlib.h>
 #include <stack>
+#include <set>
+using namespace std;
 
 class GameController
 {
-    cardDeck c;
+    cardDeck deck;
+    std::set<Card> shuffleDeck;
     std::vector<Card> lowerOne, lowerTwo, lowerThree, lowerFour, lowerFive, lowerSix, lowerSeven;
     std::stack<Card> topOne, topTwo, topThree, topFour, flipPile;
 
@@ -15,10 +17,41 @@ class GameController
 
     }
 
-    void flipCard()
+    void initSolitaire()
+    {
+        //lowerOne.insert(0, )
+    }
+
+//moves a card from its current position to a new vector
+    void moveCard(Card c, vector<Card> v)
+    {
+        v.emplace(v.end(), c);
+    }
+
+    void flipCard(Card c)
+    {
+        if(c.getVisible())
+        {
+            c.setNotVis();
+        }
+        else
+        {
+            c.setVis();
+        }
+    }
+
+//runs the GameController class
+    void run()
     {
 
     }
+
+//terminates the program
+    void stop()
+    {
+        return;
+    }
+
 
     int main()
     {
