87b41f90b8998d99a1c9385e657bf9da3a3ec882
Ian Bush <IBUSH001@csbsju.edu>
2019-02-15 17:54:51 +0000

    Expansion

--- SolitaireSimulation/MVPSolitaire/GameController.cpp
+++ SolitaireSimulation/MVPSolitaire/GameController.cpp
@@ -1,4 +1,28 @@
+#include "Card.cpp"
+#include "cardDeck.cpp"
+#include <vector>
+#include <stdlib.h>
+#include <stack>
+
 class GameController
 {
+    cardDeck c;
+    std::vector<Card> lowerOne, lowerTwo, lowerThree, lowerFour, lowerFive, lowerSix, lowerSeven;
+    std::stack<Card> topOne, topTwo, topThree, topFour, flipPile;
 
-}
+    GameController()
+    {
+
+    }
+
+    void flipCard()
+    {
+
+    }
+
+    int main()
+    {
+        return 0;
+    }
+
+};
