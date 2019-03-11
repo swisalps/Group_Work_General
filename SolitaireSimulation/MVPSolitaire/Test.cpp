5552f319568eee30cd9c1abae7ec6a3c8934914f
slaufers001 <slaufers001@csbsju.edu>
2019-03-11 20:46:02 +0000

    Put down makePiles Test

--- SolitaireSimulation/MVPSolitaire/Test.cpp
+++ SolitaireSimulation/MVPSolitaire/Test.cpp
@@ -17,17 +17,45 @@ class Test{
     Test(){
         init();
         // TEST METHODS
-        
+        test_makePiles();
     }
 
     void init(){
         // Create a Shuffled Deck
         cardDeck shuffDeck = cardDeck();
         deck = shuffDeck.shuffled;
+
+        // Make the lower Piles
+        list<Card> pile;
+        for(int i = 0; i < 7; i++){
+            pile.clear();
+            for(int j=0; j<i+1; j++){
+                pile.push_front(deck.front());
+                deck.pop_front();
+            }
+            pile.front().setVis();
+            lowerPiles[i] = pile;
+	    }
     }
+
 
     // ============ TESTS ============
-    void test_getNum(){
+    void test_makePiles(){
+        cout << "=============== NOW TESTING MAKE_PILES ===============" << endl;
 
+        // Tests whether the lower piles are initialized correctly
+        for(int i=0; i<lowerPiles.size(); i++){
+            list<Card> pile = lowerPiles[i];
+            cout << "Pile " + std::to_string(i) + " Front->: ";
+            for(list<Card>::iterator it=pile.begin(); it!=pile.end(); it++){
+                cout << it->toStringOneLine()<<", ";
+            }
+            cout << " " << endl;
+        }
     }
 };
+
+int main(){
+    new Test();
+    return 0;
+}
