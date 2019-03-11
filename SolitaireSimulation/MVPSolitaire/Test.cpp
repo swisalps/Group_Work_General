3a0b39e6a59ba9d4e2afaa243c6e2eae1537926a
slaufers001 <slaufers001@csbsju.edu>
2019-03-11 20:59:36 +0000

    Put down more tests

--- SolitaireSimulation/MVPSolitaire/Test.cpp
+++ SolitaireSimulation/MVPSolitaire/Test.cpp
@@ -13,11 +13,15 @@ class Test{
     public:
     list<Card> deck;
     array<list<Card>, 7> lowerPiles;
+    std::list<Card> topDiamonds, topSpades, topHearts, topClubs, flipPile;
 
     Test(){
         init();
         // TEST METHODS
         test_makePiles();
+        test_getVisible();
+        test_setNotVisible();
+        test_setVisible();
     }
 
     void init(){
@@ -25,7 +29,7 @@ class Test{
         cardDeck shuffDeck = cardDeck();
         deck = shuffDeck.shuffled;
 
-        // Make the lower Piles
+        // Make the lower Piles (makePile() method)
         list<Card> pile;
         for(int i = 0; i < 7; i++){
             pile.clear();
@@ -41,7 +45,7 @@ class Test{
 
     // ============ TESTS ============
     void test_makePiles(){
-        cout << "=============== NOW TESTING MAKE_PILES ===============" << endl;
+        cout << "=============== NOW TESTING MAKE_PILE ===============" << endl;
 
         // Tests whether the lower piles are initialized correctly
         for(int i=0; i<lowerPiles.size(); i++){
@@ -51,6 +55,40 @@ class Test{
                 cout << it->toStringOneLine()<<", ";
             }
             cout << " " << endl;
+        }
+    }
+    void test_getVisible(){
+        cout << "=============== NOW TESTING GET_VISIBLE ===============" << endl;
+        // Tests whether a card is visible
+        if(lowerPiles[0].front().getVisible() == true){
+            cout << "The top card of pile 0 is visible" << endl;
+        }
+        else{
+            cout << "The top card of pile 0 is NOT visible" << endl;
+        }
+    }
+    void test_setNotVisible(){
+        cout << "=============== NOW TESTING SET_NOT_VISIBLE ===============" << endl;
+        lowerPiles[0].front().setNotVis();
+
+        // Tests whether a card is visible
+        if(lowerPiles[0].front().getVisible() == true){
+            cout << "The top card of pile 0 is visible" << endl;
+        }
+        else{
+            cout << "The top card of pile 0 is NOT visible" << endl;
+        }
+    }
+    void test_setVisible(){
+        cout << "=============== NOW TESTING SET_VISIBLE ===============" << endl;
+        lowerPiles[0].front().setVis();
+
+        // Tests whether a card is visible
+        if(lowerPiles[0].front().getVisible() == true){
+            cout << "The top card of pile 0 is visible" << endl;
+        }
+        else{
+            cout << "The top card of pile 0 is NOT visible" << endl;
         }
     }
 };
