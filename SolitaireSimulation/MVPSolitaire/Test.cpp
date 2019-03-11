f2b5ad39189c42dc3cc2e2cff3ff724a7be66c2a
slaufers001 <slaufers001@csbsju.edu>
2019-03-11 18:40:26 +0000

    Started Test Class

--- SolitaireSimulation/MVPSolitaire/Test.cpp
+++ SolitaireSimulation/MVPSolitaire/Test.cpp
@@ -1,1 +1,26 @@
+#include "cardDeck.cpp"
+#include <vector>
+#include <stdlib.h>
+#include <stack>
+#include <set>
+#include <iostream>
+#include <array>
+#include <string>
+#include <chrono>
+using namespace std;
 
+class Test{
+    public:
+    cardDeck deck; // this deck is shuffled
+
+    Test(){
+
+    }
+
+    void init(){
+        // Create a Shuffled Deck
+        deck = cardDeck();
+    }
+
+    // ============ TESTS ============
+};
